5ee2a807bc07f28f557efb43fb7249927bb27b71
Danish Suleman <97557615+DanishSuleman@users.noreply.github.com>
2022-07-20 23:58:29 +0500

    Updated Models.py And Templates

--- apps/brands/models.py
+++ apps/brands/models.py
@@ -3,6 +3,6 @@ from django.db import models
 # Create your models here.
 class Brand(models.Model):
     name = models.CharField(max_length=256)
-    image = models.FileField(upload_to='home/brands')
+    image = models.FileField(upload_to='brands')
     def __str__(self):
         return self.name
--- apps/team_members/models.py
+++ apps/team_members/models.py
@@ -4,7 +4,7 @@ from django.db import models
 class Team_Member(models.Model):
     name = models.CharField(max_length=256)
     designation = models.CharField(max_length=256)
-    image = models.FileField(upload_to='about/team_members')
+    image = models.FileField(upload_to='team_members')
     facebook = models.TextField(default="https://www.facebook.com/groups/407961892610345/")
     linkedIn = models.TextField(default="https://www.linkedin.com/company/javascript-developer")
     twitter = models.TextField(default="https://twitter.com/javascript_dev_")
