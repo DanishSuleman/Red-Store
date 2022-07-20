8c4808ab023612598fa6e456a18b9672e065dfda
Danish Suleman <97557615+DanishSuleman@users.noreply.github.com>
2022-07-20 11:30:11 +0500

    Update settings.py

--- RedStore/settings.py
+++ RedStore/settings.py
@@ -120,6 +120,9 @@ STATICFILES_DIRS = [
 
 STATIC_ROOT = BASE_DIR / 'staticfiles'
 
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedStaticFilesStorage'
+
+
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
 
